9bed67a79af853b9c4b51c12fee7143fad70a7c9
Chad Schroeder <77646640+calansc@users.noreply.github.com>
2024-09-18 14:12:54 -0400

    Install express and ejs. Initial index commit

---
+++ routes/indexRouter.js
@@ -0,0 +1,23 @@
+const { Router } = require("express");
+const path = require("path");
+
+const indexRouter = Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+indexRouter.get("/", (req, res) => {
+  res.render("index");
+});
+
+module.exports = indexRouter;
