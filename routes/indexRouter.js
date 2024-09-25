87254b42842a03a745516307981d8690bef89baa
Chad Schroeder <77646640+calansc@users.noreply.github.com>
2024-09-25 14:08:30 -0400

    Add new message page

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -16,6 +16,18 @@ const messages = [
   },
 ];
 
+indexRouter.get("/new", (req, res) => {
+  res.render("form", { title: "New Message" });
+});
+indexRouter.post("/new", (req, res) => {
+  messages.push({
+    text: req.body.messageText,
+    user: req.body.messageUser,
+    added: new Date(),
+  });
+  res.redirect("/");
+});
+
 indexRouter.get("/", (req, res) => {
   res.render("index", { title: "Mini Messageboard", messages: messages });
 });
