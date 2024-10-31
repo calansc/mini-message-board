322f5b1ee78f6dad7aea3e9dbea3c67497b68f18
Chad Schroeder <77646640+calansc@users.noreply.github.com>
2024-10-31 13:56:32 -0400

    Update pool.js pool connection

--- db/pool.js
+++ db/pool.js
@@ -1,10 +1,17 @@
+require("dotenv").config();
 const { Pool } = require("pg");
+const connectionString = process.env.CONNECTION_STRING;
+const pool = new Pool({
+  connectionString,
+});
+
+module.exports = pool;
 
 // All of the following properties should be read from environment variables
 // We're hardcoding them here for simplicity
-module.exports = new Pool({
-  host: process.env.HOST, // or wherever the db is hosted
-  user: process.env.USER,
-  database: process.env.DATABASE,
-  password: process.env.PASSWORD, // the default port
-});
+// module.exports = new Pool({
+//   host: process.env.HOST, // or wherever the db is hosted
+//   user: process.env.USER,
+//   database: process.env.DATABASE,
+//   password: process.env.PASSWORD, // the default port
+// });
